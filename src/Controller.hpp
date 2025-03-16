3e3aaa42d8829525ad253c678461d54f6927d902
ChineseAegis <1962937337@qq.com>
2025-03-16 10:54:26 +0000

    first commit

---
+++ src/Controller.hpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include"Disk.hpp"
+#include<vector>
+using namespace std;
+
+class Controller{
+    public:
+    int num_T;//时间片数量
+    int num_disk;//磁盘数量 3<=N<=10
+    int num_tag;//标签数量
+    int num_v;//每个硬盘的单元数量
+    vector<Disk> disks; //存储硬盘的数组指针
+
+    Controller(int num_T,int num_disk,int num_tag,int num_v):num_T(num_T),num_disk(num_disk),num_tag(num_tag)
+    {
+        for(int i=0;i<num_disk;i++)
+        {
+            disks.push_back(Disk(num_v));
+        }
+    }
+};
---
+++ src/Object.hpp
@@ -0,0 +1,16 @@
+#include"Disk.hpp"
+#include<vector>
+
+//理解为文件
+class Object{
+    int id;//对象id,从1开始,唯一
+    int size;//对象大小,数量是几就代表要分成几个对象块
+    int tag;
+    int num_copy=3;
+    vector<vector<Block*>> blocks;//文件分为的对象块,具体如何存放,二维数组,维度3 x size
+
+    Object(int id,int size,int tag,vector<vector<Block*>> blocks):id(id),size(size),tag(tag),blocks(blocks)
+    {
+        
+    }
+};
