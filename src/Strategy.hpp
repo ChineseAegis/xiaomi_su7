8878575cbfc67b3fded30157c9a357cbf1a001a1
ChineseAegis <1962937337@qq.com>
2025-03-17 16:15:46 +0000

    修改计算token函数

--- src/Strategy.hpp
+++ src/Strategy.hpp
@@ -1,80 +1,101 @@
 #include <cmath> // 引入标准库中的 ceil 函数
 using namespace std;
-#include<deque>
-#include<string>
-#include<vector>
-#include<algorithm> // 用于max函数
+#include <deque>
+#include <string>
+#include <vector>
+#include <algorithm> // 用于max函数
 #include <stdexcept>
 
-class Strategy {
-public:
+class Strategy
+{
+
+    static int calculate_num_pre_read_action(const vector<string> &actions, int time, int index)
+    {
+        int count = 0;
+        for (int j = time; j >= 0; j--)
+        {
+            for (int i = index - 1; i >= 0; i--)
+            {
+                if (actions[j][i] == 'r')
+                {
+                    count++;
+                }
+                else
+                {
+                    return count;
+                }
+            }
+            if (j - 1 >= 0)
+            {
+                index = actions[j - 1].size();
+            }
+        }
+        return count;
+    }
     // 根据行为计算tokens的方式
-    
-    static int calculate_tokens(const vector<string>& actions, int G, int prev_token = 0) {
+
+    static int calculate_tokens(const string &actions, int G, const vector<string> &all_actions, int time)
+    {
         int token_count = 0;
         bool has_jed = false; // 标记是否已经执行了j动作
 
-        for (size_t i = 0; i < actions.size(); i++) {
-            const string& action = actions[i]; // 获取当前时间片的动作序列
+        if (actions[0] == 'j')
+        {
+            token_count += G;
+            return token_count;
+        }
 
-            if (action.find('j') != string::npos) {
-                if (i != 0) {
-                    // j只能在时间片的开始执行
-                    throw invalid_argument("j action can only be executed at the beginning of a time slice.");
-                }
-                token_count += G;
-                has_jed = true;
-                // j执行后，该时间片不能有其他动作
-                if (action.length() > 1) {
-                    throw invalid_argument("No other actions are allowed after j in the same time slice.");
-                }
-            } else if (action.find('p') != string::npos) {
-                if (has_jed) {
-                    throw invalid_argument("No other actions are allowed after j in the same time slice.");
-                }
+        for (size_t i = 0; i < actions.size(); i++)
+        {
+            const char &action = actions[i]; // 获取当前时间片的动作序列
+
+            if (action == 'p')
+            {
                 token_count += 1;
-            } else if (action.find('r') != string::npos) {
-                if (has_jed) {
-                    throw invalid_argument("No other actions are allowed after j in the same time slice.");
+            }
+            else if (action == 'r')
+            {
+                int pre_num_r = calculate_num_pre_read_action(all_actions, time, i);
+                // 时间片的第一个r动作
+                if (pre_num_r)
+                {
+                    token_count += max(16, static_cast<int>(ceil(64 * pow(0.8, pre_num_r))));
                 }
-                if (i == 0) {
-                    // 时间片的第一个r动作
-                    if (prev_token == 0) {
-                        token_count += 64;
-                    } else {
-                        token_count += max(16, static_cast<int>(ceil(prev_token * 0.8)));
-                    }
-                } else {
-                    if (actions[i - 1].find('r') == string::npos) {
-                        token_count += 64;
-                    } else {
-                        token_count += max(16, static_cast<int>(ceil(prev_token * 0.8)));
-                    }
+                else
+                {
+                    token_count += 64;
                 }
-            } else {
+            }
+            else
+            {
                 throw invalid_argument("Invalid action in time slice: " + action);
             }
         }
         return token_count;
     }
+    public:
 
     // 更新对应时间片的token消耗
     // 输入的 actions 是时间片序列，每个时间片包含一个动作序列
     // tokens 是更新之前记录的每个时间片消耗的 token 数量
     // time 是指定时间片，若未指定，则更新所有时间片的 token
     // 返回值：返回更新后的 tokens
-    static vector<int> recalculate_tokens(const vector<vector<string>>& actions, vector<int> tokens, int G, int time = -1) {
-        if (time == -1) {
+    static vector<int> recalculate_tokens(const vector<string> &all_actions, vector<int> tokens, int G, int time = -1)
+    {
+        if (time == -1)
+        {
             // 更新所有时间片
-            for (size_t i = 0; i < actions.size(); i++) {
-                int prev_token = (i == 0) ? 0 : tokens[i - 1];
-                tokens[i] = calculate_tokens(actions[i], G, prev_token);
+            for (size_t i = 0; i < all_actions.size(); i++)
+            {
+                tokens[i] = calculate_tokens(all_actions[i], G, all_actions, i);
             }
-        } else {
+        }
+        else
+        {
             // 更新指定时间片
-            if (time >= 0 && time < static_cast<int>(actions.size())) {
-                int prev_token = (time == 0) ? 0 : tokens[time - 1];
-                tokens[time] = calculate_tokens(actions[time], G, prev_token);
+            if (time >= 0 && time < static_cast<int>(all_actions.size()))
+            {
+                tokens[time] = calculate_tokens(all_actions[time], G, all_actions, time);
             }
         }
         return tokens;
