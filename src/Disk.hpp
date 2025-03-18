19bc2cb0182b0aee0e7d361434f97ba99c6c2369
ChineseAegis <1962937337@qq.com>
2025-03-18 03:27:46 +0000

    规范化函数声明和实现

--- src/Disk.hpp
+++ src/Disk.hpp
@@ -26,7 +26,7 @@ public:
     vector<Block *> storageUnits; // 当前硬盘的存储单元情况,nullptr表示当前单元格空闲,否则指向具体存放的block
     int head = 0;                 // 磁头位置，从0开始
     int num_free_unit;
-    vector<pair<int, int>> free_units;
+    vector<pair<int, int>> free_units;//记录空闲存储单元的数组，二元组第一个元素是空闲地址的起始位置，第二个元素是空闲存储单元长度
 
     Disk(int num_v) : num_v(num_v), num_free_unit(num_v)
     {
@@ -37,25 +37,44 @@ public:
         free_units.push_back(make_pair(0, num_v));
     }
 
-    int get_block_in_which_free_unit(int index)
+    //查询索引所处第几个空闲段
+    int get_block_in_which_free_unit(int index);
+
+    //将两个存储单元的所有空闲段合并为一个空闲端，前提是索引直接的存储单元都是空闲的
+    bool merge_two_free_units(int begin, int end);
+};
+
+int Disk::get_block_in_which_free_unit(int index)
+{
+    if(storageUnits[index]){
+        return -1;
+    }
+    for (int i = 0; i < free_units.size(); i++)
     {
-        for (int i = 0; i < free_units.size(); i++)
+        if (i >= free_units[i].first && i < free_units[i].second + free_units[i].first)
         {
-            if (i >= free_units[i].first && i < free_units[i].second + free_units[i].first)
-            {
-                return i;
-            }
+            return i;
         }
     }
+}
 
-    bool merge_two_free_units(int begin, int end)
+bool Disk::merge_two_free_units(int begin, int end)
     {
         if(begin>end)return false;
         for(int i = begin; i < end + 1; i++){
-
+            if(storageUnits[i]){
+                return false;
+            }
         }
         int first = get_block_in_which_free_unit(begin);
         int second = get_block_in_which_free_unit(end);
 
+        int length = free_units[second].first+free_units[second].second;
+        free_units[first].second+=length;
+        auto i1 = free_units.begin()+first+1;
+        auto i2 = free_units.begin()+second+1;
+        free_units.erase(i1,i2);
+        return true;
+
     }
-};
+
