38cae8956d351f5dc97ab2c8efb1fc2d4878077e
ChineseAegis <1962937337@qq.com>
2025-03-18 04:56:31 +0000

    更改存储单元变量名称

--- src/Disk.hpp
+++ src/Disk.hpp
@@ -8,11 +8,12 @@ struct Block
 {
     int size = 1; // 对象块大小,当前默认等于1,不用修改
 
+    int id;//对象块id，代表该对象块是object的第几个块
     int disk_id;   // 对象块所属的硬盘id
     int index;     // 对象块在硬盘中的位置，从0开始
     int object_id; // block所属的object id
 
-    Block(int disk_id, int index, int object_id) : disk_id(disk_id), index(index), object_id(object_id)
+    Block(int disk_id, int index, int object_id,int id) : disk_id(disk_id), index(index), object_id(object_id),id(id)
     {
     }
 };
@@ -23,7 +24,7 @@ class Disk
 public:
     int id;                       // 唯一,从0开始
     int num_v;                    // 硬盘存储单元数量,也表示最多可以存储的对象块的数量
-    vector<Block *> storageUnits; // 当前硬盘的存储单元情况,nullptr表示当前单元格空闲,否则指向具体存放的block
+    vector<Block *> units; // 当前硬盘的存储单元情况,nullptr表示当前单元格空闲,否则指向具体存放的block
     int head = 0;                 // 磁头位置，从0开始
     int num_free_unit;
     vector<pair<int, int>> free_units;//记录空闲存储单元的数组，二元组第一个元素是空闲地址的起始位置，第二个元素是空闲存储单元长度
@@ -32,7 +33,7 @@ public:
     {
         for (int i = 0; i < num_v; i++)
         {
-            storageUnits.push_back(nullptr);
+            units.push_back(nullptr);
         }
         free_units.push_back(make_pair(0, num_v));
     }
@@ -46,7 +47,7 @@ public:
 
 int Disk::get_block_in_which_free_unit(int index)
 {
-    if(storageUnits[index]){
+    if(units[index]){
         return -1;
     }
     for (int i = 0; i < free_units.size(); i++)
@@ -62,7 +63,7 @@ bool Disk::merge_two_free_units(int begin, int end)
     {
         if(begin>end)return false;
         for(int i = begin; i < end + 1; i++){
-            if(storageUnits[i]){
+            if(units[i]){
                 return false;
             }
         }
